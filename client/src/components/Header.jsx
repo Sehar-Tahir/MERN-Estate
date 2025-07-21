8c2bde84e3ad05b27d662fdb01eaa78b1bb08b31
Sehar-Tahir <sehartahirofficial@gmail.com>
2025-07-21 16:31:37 +0500

    font family updated

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,15 +5,15 @@ const Header = () => {
     return (
         <>
             <header className="bg-slate-200 shadow-md ">
-                <div className="flex  items-center justify-between max-w-6xl mx-auto p-3">
+                <div className="flex items-center justify-between max-w-6xl mx-auto p-3">
                     <Link to='/'>
                         <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
                             <span className="text-slate-500">Elite</span>
                             <span className="text-slate-700">Estate</span>
                         </h1>
                     </Link>
-                    <form className="bg-slate-100 p-3 rounded-lg flex items-center">
-                        <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-44 sm:w-64" />
+                    <form className="bg-slate-100 p-2.5 rounded-lg flex items-center">
+                        <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-64" />
                         <FaSearch className="text-slate-600" />
                     </form>
                     <ul className="flex gap-4">
