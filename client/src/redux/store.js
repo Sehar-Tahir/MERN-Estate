e9d14b21788ba93d40243032811334d6a5c6c736
Sehar-Tahir <sehartahirofficial@gmail.com>
2025-08-04 21:02:10 +0500

    redux persist added

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import  userReducer from './user/userSlice'
+import {persistReducer, persistStore} from 'redux-persist'
+import {storage} from 'redux-persist/lib/storage'
+const rootReducer = combineReducers({
+    user: userReducer,
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 
 export const store = configureStore({
     reducers: {
-        user: userReducer,
+        user: persistedReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false,
     })
 })
+
+export const persistor = persistStore(store)
+
+//persist is basically used to maintain the redux states after refreshing page
